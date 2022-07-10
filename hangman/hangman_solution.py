34fa545aa19cc86ebb61034694ba0eb4452dc485
Kelechi Daniels <zamar@Kelechis-MacBook-Pro.local>
2022-07-10 06:23:21 +0100

    adds input feature

--- hangman/hangman_solution.py
+++ hangman/hangman_solution.py
@@ -73,9 +73,20 @@ class Hangman:
         2. If the character is a single character
         If it passes both checks, it calls the check_letter method.
         '''
-        # TODO 1: Ask the user for a letter iteratively until the user enters a valid letter
-        # TODO 1: Assign the letter to a variable called `letter`
-        # TODO 1: The letter has to comply with the following criteria: It has to be a single character. If it is not, print "Please, enter just one character"
+
+        letter = ''
+        while len(letter) != 1:
+            letter = input("Please, enter just one character: \n")
+        else:
+            print(f'You entered "{letter}"')
+        # print(letter)
+
+
+
+
+        # TODO 1: Ask the user for a letter iteratively until the user enters a valid letter - completed
+        # TODO 1: Assign the letter to a variable called `letter` - completed
+        # TODO 1: The letter has to comply with the following criteria: It has to be a single character. If it is not, print "Please, enter just one character" - completed
         # TODO 2. It has to be a letter that has not been tried yet. Use the list_letters attribute to check this. If it has been tried, print "{letter} was already tried".
         # TODO 3: If the letter is valid, call the check_letter method
         pass
@@ -83,7 +94,11 @@ class Hangman:
 def play_game(word_list):
     # As an aid, part of the code is already provided:
     game = Hangman(word_list, num_lives=5)
-    # TODO 1: To test this task, you can call the ask_letter method
+    game.ask_letter()
+
+
+
+    # TODO 1: To test this task, you can call the ask_letter method - completed
     # TODO 2: To test this task, upon initialization, two messages should be printed 
     # TODO 3: To test this task, you call the ask_letter method and check if the letter is in the word
     
